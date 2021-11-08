79192738c7b065665085bc5b2ad2051508138c16
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-11-08 18:43:18 +0200

    Factions Variable Update

--- resources/variables/factions.js
+++ resources/variables/factions.js
@@ -1,4 +1,30 @@
 const FactionInfo = {
-
+    Name: {},
+    Spawn: {
+        x: {},
+        y: {},
+        z: {}
+    },
+    Enter: {
+        x: {},
+        y: {},
+        z: {}
+    },
+    Exit: {
+        x: {},
+        y: {},
+        z: {}
+    },
+    Interior_Id: {},
+    Skin_Id: {},
+    Ranks: {
+        _1_: {},
+        _2_: {},
+        _3_: {},
+        _4_: {},
+        _5_: {},
+        _6_: {},
+        _7_: {}
+    }
 }
-global. FactionInfo = FactionInfo;
+global.FactionInfo = FactionInfo;
